22bd1098d52d3da9e3b2d510a221947198b19ff0
FJeanC <jeanfagiani1@gmail.com>
2022-06-28 10:51:32 -0300

    Changing the ouput message and verifying if the input is numeric

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ layout = [
 window = sg.Window(title, layout)
 
 def convert_km_to_mile(value):
-    return value * 0.621371
+    return value * 0.62137
 
 def convert_kg_to_pound(value):
     return value * 2.205
@@ -26,14 +26,21 @@ while True:
     if event == sg.WIN_CLOSED:
         break
     if event == "-CONVERT-":
-        user_input = float(values["-INPUT-"])
-        conversion = values["-UNITS-"]
-        if conversion == 'Km to mile':
-            result = convert_km_to_mile(user_input)
-        elif conversion == 'Kg to pound':
-            result = convert_kg_to_pound(user_input)
+        user_input = values["-INPUT-"]
+        if user_input.isnumeric():
+            conversion = values["-UNITS-"]
+            user_input = float(user_input)
+            if conversion == 'Km to mile':
+                result = convert_km_to_mile(user_input)
+                output_string = "{} Km is {:.5f} miles.".format(user_input, result)
+            elif conversion == 'Kg to pound':
+                result = convert_kg_to_pound(user_input)
+                output_string = "{} Kg is {:.5f} pounds.".format(user_input, result)
+            else:
+                result = convert_sec_to_min(user_input)
+                output_string = "{} seconds is {:.5f} minutes.".format(user_input, result)
+            window["-OUTPUT-"].update(output_string)
         else:
-            result = convert_sec_to_min(user_input)
-        window["-OUTPUT-"].update(result)
+            window["-OUTPUT-"].update("Please enter a number.")
 
 window.close()
